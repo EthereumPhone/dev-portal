c3a15cb696e9b420dcce4b8e8ec210628b1587dc
Stipe <slatkovi@gmail.com>
2021-12-20 21:34:35 -0500

    home redirect

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: false
+      },
+    ]
+  }
 }
