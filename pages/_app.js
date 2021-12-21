3133b7c0cc1446ca17f7835174bfb653fa57f176
Stipe <slatkovi@gmail.com>
2021-12-20 21:34:51 -0500

    plugging in main layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import AppLayout from '../components/AppLayout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AppLayout>
+      <Component {...pageProps} />
+    </AppLayout>
+  )
 }
 
 export default MyApp
