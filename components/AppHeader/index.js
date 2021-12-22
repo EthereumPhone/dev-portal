97668aaf099a5680c324e4587da92b3714ada089
Stipe <slatkovi@gmail.com>
2021-12-21 23:20:48 -0500

    header style update

--- components/AppHeader/index.js
+++ components/AppHeader/index.js
@@ -2,7 +2,7 @@ import Logo from '../Logo'
 import styles from './index.module.css'
 
 const AppHeader = () => (
-  <div className={styles['app-header']}>
+  <div className={styles.container}>
     <Logo
       className={styles.logo}
     />
