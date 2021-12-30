6eccc8eb5a35a80df315d459b297c8dd6305e7d4
Stipe <slatkovi@gmail.com>
2021-12-30 10:24:07 -0500

    adding description to the card component

--- components/Card/index.js
+++ components/Card/index.js
@@ -2,18 +2,27 @@ import styles from './index.module.css'
 import BackButton from '../BackButton'
 
 
-const Card = ({ isBackEnabled = false, title, children }) => (
+const Card = ({ isBackEnabled = false, title, description, children }) => (
   <div className={styles.container}>
     
     <div className={styles.header}>
+
       {isBackEnabled &&
-        <BackButton />
+        <BackButton className={styles.back} />
       }
-      {title &&
-        <span className={styles.title}>
-          {title}
-        </span>
-      }
+
+      <div className={styles.info}>
+        {title &&
+          <span className={styles.title}>
+            {title}
+          </span>
+        }
+        {description &&
+          <span className={styles.description}>
+            {description}
+          </span>
+        }
+      </div>
     </div>
 
     <div className={styles.children}>
