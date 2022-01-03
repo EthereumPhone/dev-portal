e46009f4d8374c7db312fceec783aeb1ebcd2db7
emunsi <31884918+emunsi@users.noreply.github.com>
2022-01-03 13:45:34 +0100

    Changes based on PR

--- components/AppHeader/index.js
+++ components/AppHeader/index.js
@@ -1,12 +1,10 @@
 import Logo from '../Logo'
-import User from '../User'
 import styles from './index.module.css'
 
 
 const AppHeader = () => (
   <div className={styles['app-header']}>
     <Logo />
-    <User />
   </div>
 )
 
--- components/Button/index.js
+++ components/Button/index.js
@@ -1,14 +1,19 @@
 import styles from './index.module.css'
 
 export const ButtonType = {
-  PRIMARY: 'PRIMARY'
+  PRIMARY: 'PRIMARY',
+  SECONDARY: 'SECONDARY',
+
 }
 
-const Button = ({ label, onClick }) => (
+const ButtonStyle = {
+  [ButtonType.PRIMARY]: styles.primary,
+  [ButtonType.SECONDARY]: styles.secondary
+}
+const Button = ({ type = ButtonType.PRIMARY, label, onClick, className }) => (
   <button
-    className={styles.container}
-    onClick={onClick}>
-    {label}
+    className={`${styles.container} ${ButtonStyle[type]} ${className}`}>
+      Continue
   </button>
 )
 
