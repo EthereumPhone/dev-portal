bea1ce712d51f72bd978b296266d03adfbb953e5
Stipe <slatkovi@gmail.com>
2022-01-04 20:44:54 -0500

    app listed message

--- pages/app/list/index.js
+++ pages/app/list/index.js
@@ -4,33 +4,56 @@ import ConnectedPage from '../../../components/ConnectedPage'
 import Image from 'next/image'
 import emptyIcon from './empty.svg'
 import appIconSrc from './app.svg'
+import checkSrc from './check.svg'
 import styles from './index.module.css'
 
 
 const EmptyView = () => (
   <div className={styles.empty}>
     <Image src={emptyIcon} alt="empty" />
-    <p className={styles['empty-text']}>
+    <p className={styles.empty_text}>
       You haven’t listed any dApps yet.<br/>
       Click below to get started. 
     </p>
   </div>
 )
 
+const SubmitMessage = () => (
+  <div className={styles.submit_message}>
+    <div className={styles.submit_check_wrapper}>
+      <Image
+        src={checkSrc}
+        alt="checked"
+      />
+    </div>
+    <div className={styles.submit_message_text_wrapper}>
+      <span
+        className={styles.submit_message_title}>
+          dApp Successfully Submitted
+      </span>
+      <span
+        className={styles.submit_message_description}>
+          Please allow a few moments for your dApp to appear here.
+      </span>
+    </div>
+  </div>
+)
+
 const AppItem = ({ logoSrc = appIconSrc, name, category, description }) => (
-  <div className={styles['app-item']}>
+  <div className={styles.app_item}>
     <Image
-      className={styles['app-item-logo']}
+      className={styles.app_item_logo}
       src={logoSrc}
+      alt="app logo"
     />
-    <div className={styles['app-item-info']}>
-      <span className={styles['app-item-name']}>
+    <div className={styles.app_item_info}>
+      <span className={styles.app_item_name}>
         {name}
       </span>
-      <span className={styles['app-item-category']}>
+      <span className={styles.app_item_category}>
         {category}
       </span>
-      <p className={styles['app-item-description']}>
+      <p className={styles.app_item_description}>
         {description}
       </p>
     </div>
@@ -38,7 +61,8 @@ const AppItem = ({ logoSrc = appIconSrc, name, category, description }) => (
 )
 
 const ListView = ({ items }) => (
-  <div className={styles['list-view']}>
+  <div className={styles.list_view}>
+    <SubmitMessage />
     {items.map((item, index) => (
       <AppItem
         key={`app-item-${index}`}
@@ -73,7 +97,7 @@ const ListedApps = () => (
     <Card
       title="Listed dApps"
       className={styles.card}
-      headerClassName={styles['card-header']}>
+      headerClassName={styles.card_header}>
 
       <div className={styles.body}>
 
