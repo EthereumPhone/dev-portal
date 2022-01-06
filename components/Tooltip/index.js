e40ffc3d8206ec254992b1c684ed0492658d24b3
Stipe <slatkovi@gmail.com>
2022-01-05 20:17:15 -0500

    tooltip

---
+++ components/Tooltip/index.js
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import styles from './index.module.css'
+
+
+const Tooltip = ({ delayInMs = 400, children, content }) => {
+
+  let timeout
+
+  const [isActive, setIsActive] = useState(false)
+
+  const show = () => {
+    timeout = setTimeout(() => {
+      setIsActive(true)
+    }, delayInMs)
+  }
+
+  const hide = () => {
+    clearInterval(timeout)
+    setIsActive(false)
+  }
+
+  return (
+    <div
+      className={styles.container}
+      onMouseEnter={show}
+      onMouseLeave={hide}>
+
+      {children}
+
+      {isActive && (
+        <div className={styles.content}>
+          {content}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Tooltip
