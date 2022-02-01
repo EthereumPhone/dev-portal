b1fe72311c40f69a8cbb50d8e80f63b987ef8783
Stipe <slatkovi@gmail.com>
2022-02-01 14:04:53 -0500

    apk uploads ftw

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { WalletProvider } from '../components/WalletProvider'
 import { ToastContextProvider } from '../components/Toast'
 import '../styles/globals.css'
 import '../components/WalletProvider/style.css'
+import 'regenerator-runtime/runtime.js'
 
 
 function MyApp({ Component, pageProps }) {
