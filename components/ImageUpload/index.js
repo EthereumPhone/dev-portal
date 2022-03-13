9fe91f55044cae7b9ab12b89cd77eb1fce48fbad
Stipe <slatkovi@gmail.com>
2022-03-12 21:43:20 -0500

    Disabled logic for image upload

--- components/ImageUpload/index.js
+++ components/ImageUpload/index.js
@@ -1,18 +1,24 @@
 import Image from 'next/image'
+import classes from 'classnames'
 import FileInput from '../FileInput'
 import iconSrc from './icon.svg'
 import styles from './index.module.css'
 
-const ImageUpload = ({ className, onUpload, image }) => (
+const ImageUpload = ({ className, imageClassName, onUpload, image, isDisabled = false }) => (
   <FileInput
-    className={`${styles.container} ${className}`}
+    className={classes(
+      styles.container,
+      className,
+      { [styles.disabled]: isDisabled }
+    )}
     onFileUpload={files => onUpload(files[0])}
+    isDisabled={isDisabled}
     accept=".png, .jpg, .jpeg">
 
     {!!image &&
       <img
         src={URL.createObjectURL(image)}
-        className={styles.image}
+        className={classes(styles.image, imageClassName)}
         alt="uploaded image"
       />
     }
