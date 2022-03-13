dbea3fc5e18d0cd5ba16d76d207f824d4418ccab
Stipe <slatkovi@gmail.com>
2022-03-12 22:36:18 -0500

    screenshot upload

--- components/AppInputCard/AppScreens/index.js
+++ components/AppInputCard/AppScreens/index.js
@@ -1,17 +1,39 @@
 import Button, { ButtonType } from '../../Button'
 import ImageUpload from '../../ImageUpload'
+import { useToast } from '../../Toast'
+import { addFile } from '../../../clients/ipfs.js'
 import styles from './index.module.css'
 
 
-const AppScreens = ({ screenshotFiles, setScreenshotFiles, onBack, onNext }) => {
+const AppScreens = ({ screenshotFiles, setScreenshotFiles, screenshotCids, setScreenshotCids, onBack, onNext }) => {
+
+  const toastIt = useToast()
 
   const screenshotFilesCount = Object.keys(screenshotFiles).length
-
   const isComplete = screenshotFilesCount > 0
 
+  const tryAddScreenshot = ({ key, file }) => {
+    try {
+      addScreenshot({ key, file })
+    } catch (err) {
+      toastIt({ message: 'Could not upload screenshot. Please try again.' })
+    }
+  }
+
   const addScreenshot = ({ key, file }) => {
-    const updatedScreenshots = { ...screenshotFiles, [key]: file }
-    setScreenshotFiles(updatedScreenshots)
+    const { cid } = await addFile(file)
+    updateScreenshotFiles({ key, file })
+    updateScreenshotCids({ key, cid })
+  }
+
+  const updateScreenshotFiles = ({ key, file }) => {
+    const updatedFiles = { ...screenshotFiles, [key]: file }
+    setScreenshotFiles(updatedFiles)
+  }
+
+  const updateScreenshotCids = ({ key, cid }) => {
+    const updatedCids = { ...screenshotCids, [key]: cid }
+    setScreenshotCids(updatedCids)
   }
 
   return (
@@ -25,20 +47,20 @@ const AppScreens = ({ screenshotFiles, setScreenshotFiles, onBack, onNext }) => 
         <ImageUpload
           className={styles.upload}
           imageClassName={styles.screenshot}
-          onUpload={file => addScreenshot({ key: 0, file })}
+          onUpload={file => tryAddScreenshot({ key: 0, file })}
           image={screenshotFiles[0]}
         />
         <ImageUpload
           className={styles.upload}
           imageClassName={styles.screenshot}
-          onUpload={file => addScreenshot({ key: 1, file })}
+          onUpload={file => tryAddScreenshot({ key: 1, file })}
           image={screenshotFiles[1]}
           isDisabled={screenshotFilesCount < 1}
         />
         <ImageUpload
           className={styles.upload}
           imageClassName={styles.screenshot}
-          onUpload={file => addScreenshot({ key: 2, file })}
+          onUpload={file => tryAddScreenshot({ key: 2, file })}
           image={screenshotFiles[2]}
           isDisabled={screenshotFilesCount < 2}
         />
